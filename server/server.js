b1a5317cf41d86e221efb5cd7bf424894e64cd41
LightFromAStone <github@lightfromastone.me>
2022-02-23 12:07:22 -0700

    fixed typo

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path'); // directory path. Used for heroku
 const app = express();
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../indes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../index.html')));
 
 const SERVER_PORT = process.env.PORT || 4005; // heroku has their own env file that we are calling on here
 
